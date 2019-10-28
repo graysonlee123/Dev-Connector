83f5dc211a98d0b6d95f17a3d67b8a8c65208cca
graysonlee123 <graysonleegantek@gmail.com>
2019-10-28 12:10:28 -0400

    clears profile on logout and works on dashboard conditional rendering

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,10 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-user'></i>{' '}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
         <Link onClick={logout} to='!#'>
           <i className='fas fa-sign-out-alt'></i>{' '}
           <span className='hide-sm'>Logout</span>
