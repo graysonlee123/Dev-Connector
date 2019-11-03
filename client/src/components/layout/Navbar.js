c8c66f3f97d06a04429b8c8e00117250aa28c791
graysonlee123 <graysonleegantek@gmail.com>
2019-11-03 18:15:33 -0500

    profiles route and base component logic

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,14 +8,19 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/profiles'>Developers</Link>
+      </li>
+      <li>
         <Link to='/dashboard'>
           <i className='fas fa-user'></i>{' '}
           <span className='hide-sm'>Dashboard</span>
         </Link>
-        <Link onClick={logout} to='!#'>
+      </li>
+      <li>
+        <a onClick={logout} to='#!'>
           <i className='fas fa-sign-out-alt'></i>{' '}
           <span className='hide-sm'>Logout</span>
-        </Link>
+        </a>
       </li>
     </ul>
   );
@@ -23,7 +28,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <Link to='#!'>Developers</Link>
+        <Link to='/profiles'>Developers</Link>
       </li>
       <li>
         <Link to='/register'>Register</Link>
