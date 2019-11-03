eb1e90ee127a7a1269addf6202f5fb405375ae2c
graysonlee123 <graysonleegantek@gmail.com>
2019-11-03 17:37:32 -0500

    delete account profile action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from './types';
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  UPDATE_PROFILE,
+  CLEAR_PROFILE,
+  ACCOUNT_DELETED
+} from './types';
 
 export const getCurrentProfile = () => async dispatch => {
   try {
@@ -158,5 +164,32 @@ export const deleteEducation = id => async dispatch => {
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status }
     });
+  }
+};
+
+// Delete account and profile
+export const deleteAccount = () => async dispatch => {
+  if (
+    window.confirm(
+      'Are you sure you want to delete your account? This cannot be undone.'
+    )
+  ) {
+    try {
+      const res = await axios.delete('/api/profile/');
+
+      dispatch({
+        type: CLEAR_PROFILE
+      });
+      dispatch({
+        type: ACCOUNT_DELETED
+      });
+
+      dispatch(setAlert('Your account has been permanantly deleted.'));
+    } catch (err) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
   }
 };
